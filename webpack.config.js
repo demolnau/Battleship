905e30ddfd67cb0c6dd7e93984d181f06739a33f
Devin <demolnau@iastate.edu>
2023-06-20 16:14:48 -0500

    update webpack and html file

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/javascript.js',
+  entry: './src/game_play.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
