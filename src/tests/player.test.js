feb15850cd3bb6510b9af633f7498b2e8c81b79e
Devin <demolnau@iastate.edu>
2023-06-20 15:45:41 -0500

    update tests

--- src/tests/player.test.js
+++ src/tests/player.test.js
@@ -1,15 +1,17 @@
 const Player = require("../factories/player.js")
 var player4 = Player();
 var player3 = Player();
-player4.gameboard.randomly_place_ships();
-player3.gameboard.placeShip(player3.gameboard.ships[0],[[1,2],[1,3],[1,4],[1,5],[1,6]])
-player3.gameboard.placeShip(player3.gameboard.ships[1],[[2,3],[2,4],[2,5],[2,6]])
-player3.gameboard.placeShip(player3.gameboard.ships[2],[[8,5],[8,6],[8,7]])
-player3.gameboard.placeShip(player3.gameboard.ships[3],[[7,3],[8,3],[9,3]])
-player3.gameboard.placeShip(player3.gameboard.ships[4],[[4,4],[4,5]])
-player4.gameboard.get_number_of_ships_placed();
-player3.gameboard.get_number_of_ships_placed();
 
+beforeAll(()=>{
+    player4.gameboard.randomly_place_ships();
+    player3.gameboard.placeShip(player3.gameboard.ships[0],[[1,2],[1,3],[1,4],[1,5],[1,6]])
+    player3.gameboard.placeShip(player3.gameboard.ships[1],[[2,3],[2,4],[2,5],[2,6]])
+    player3.gameboard.placeShip(player3.gameboard.ships[2],[[8,5],[8,6],[8,7]])
+    player3.gameboard.placeShip(player3.gameboard.ships[3],[[7,3],[8,3],[9,3]])
+    player3.gameboard.placeShip(player3.gameboard.ships[4],[[4,4],[4,5]])
+    player4.gameboard.get_number_of_ships_placed();
+    player3.gameboard.get_number_of_ships_placed();
+});
 
 test("check the number of available positions to hit",()=>{
     expect(player4.available_moves.length).toBe(100)
